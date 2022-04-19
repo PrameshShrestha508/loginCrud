e69afb6d8df7d50fffed87f4deec625beb579952
pramesh shrestha <prameshshrestha455@gmail.com>
2022-04-19 17:23:21 +0545

    add router

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ import express from "express";
 import conn from "./db/conn.js";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-
+import userRoutes from "./routes/users.js";
 const app = express();
 dotenv.config();
 conn;
 // import routes
-// const productRoutes = require("./routes/product");
+// const productRoutes = require("./routes/users.js");
 
 // Middleware
 // app.use(express.json());
-// app.use("/api/products/", productRoutes);
+app.use("users/", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 // App Listen
